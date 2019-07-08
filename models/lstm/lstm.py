3ad042c20055bb70841d90dc652510ab916475b4
May Helen Storvik <mayhelenrs@icloud.com>
2019-07-08 15:59:42 +0200

    Create recurrent neural net with LSTM

---
+++ models/lstm/lstm.py
@@ -0,0 +1,230 @@
+import os
+import sys
+from numpy import concatenate
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_absolute_error
+from tensorflow.python.keras import Sequential, optimizers
+from tensorflow.python.keras.layers import LSTM, Dense
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../../helpers")
+from helpers import (
+    normalize_dataset,
+    preprocess,
+    map_bool_to_int,
+    split_dataframe,
+)
+
+
+def build_model(train_dataset, train_labels, test_dataset, test_labels):
+    model = Sequential()
+    model.add(
+        LSTM(5, input_shape=(train_dataset.shape[1], train_dataset.shape[2]))
+    )
+
+    model.add(Dense(1))
+    optimizer = optimizers.Adam(lr=0.01)
+    model.compile(
+        loss="mean_squared_error",
+        optimizer=optimizer,
+        metrics=["mean_absolute_error", "mean_squared_error"],
+    )
+
+    history = model.fit(
+        train_dataset,
+        train_labels,
+        epochs=100,
+        batch_size=20,
+        validation_data=(test_dataset, test_labels),
+        verbose=0,
+        shuffle=False,
+    )
+
+    return model, history
+
+
+def reshape_df(np_array):
+    np_array = np.asarray(np_array)
+    np_array = np_array.reshape((np_array.shape[0], 1, np_array.shape[1]))
+    return np_array
+
+
+def plot_history(history):
+    hist = pd.DataFrame(history.history)
+    hist["epoch"] = history.epoch
+
+    plt.figure()
+    plt.xlabel("Epoch")
+    plt.ylabel("Mean Abs Error [MPG]")
+    plt.plot(hist["epoch"], hist["mean_absolute_error"], label="Train Error")
+    plt.plot(hist["epoch"], hist["val_mean_absolute_error"], label="Val Error")
+    # plt.ylim([0,5])
+    plt.legend()
+
+    plt.figure()
+    plt.xlabel("Epoch")
+    plt.ylabel("Mean Square Error [$MPG^2$]")
+    plt.plot(hist["epoch"], hist["mean_squared_error"], label="Train Error")
+    plt.plot(hist["epoch"], hist["val_mean_squared_error"], label="Val Error")
+    # plt.ylim([0,20])
+    plt.legend()
+    plt.show()
+
+
+def create_train_dataset(supervised_scaled, test_period):
+    train = supervised_scaled.iloc[:-test_period]
+    test = supervised_scaled.iloc[-test_period:]
+
+    train_dataset, train_labels = split_dataframe(train, ["Canteen"])
+    test_dataset, test_labels = split_dataframe(test, ["Canteen"])
+
+    train_dataset = reshape_df(train_dataset)
+    test_dataset = reshape_df(test_dataset)  # reshape_df(test_dataset)
+    train_labels = np.asarray(train_labels)
+    test_labels = np.asarray(test_labels)
+
+    return train_dataset, test_dataset, train_labels, test_labels
+
+
+def lstm(test_period):
+    df = pd.read_csv("../../data/ml_df.csv", index_col="date")
+    df.fillna(0, inplace=True)
+    df.dropna(inplace=True)
+    df.inneklemt = df.inneklemt.astype(float)
+
+    # df = df.drop(['canteen_week_ago', 'canteen_day_ago'], axis=1)
+    df = df.drop(["canteen_day_ago"], axis=1)
+
+    supervised = df.copy()
+    supervised["canteen_yesterday"] = supervised["Canteen"]
+    supervised["canteen_yesterday"] = supervised["canteen_yesterday"].shift(1)
+    supervised = supervised[1:]
+    supervised.columns = [
+        "precipitation",
+        "Canteen",
+        "holiday",
+        "vacation",
+        "inneklemt",
+        "canteen_week_ago",
+        "dist_start_year",
+        "preferred_work_temp",
+        "stay_home_temp",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+        "canteen_yesterday",
+    ]
+    supervised = supervised[
+        [
+            "Canteen",
+            "precipitation",
+            "holiday",
+            "vacation",
+            "inneklemt",
+            "canteen_week_ago",
+            "dist_start_year",
+            "preferred_work_temp",
+            "stay_home_temp",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
+            "canteen_yesterday",
+        ]
+    ]
+
+    values = supervised.astype("float64")
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled = scaler.fit_transform(values)
+
+    supervised_scaled = pd.DataFrame(scaled)
+    supervised_scaled.columns = [
+        "Canteen",
+        "precipitation",
+        "holiday",
+        "vacation",
+        "inneklemt",
+        "canteen_week_ago",
+        "dist_start_year",
+        "preferred_work_temp",
+        "stay_home_temp",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+        "canteen_yesterday",
+    ]
+
+    train_dataset, test_dataset, train_labels, test_labels = create_train_dataset(
+        supervised_scaled, test_period
+    )
+
+    model, history = build_model(
+        train_dataset, train_labels, test_dataset, test_labels
+    )
+
+    return model, scaler, test_dataset, test_labels, history
+
+
+def predict_future_for_local_testing():
+    model, scaler, test_dataset, test_labels, history = lstm(175)
+
+    # From tutorial https://machinelearningmastery.com/multivariate-time-series-forecasting-lstms-keras/
+
+    yhat = model.predict(test_dataset)
+    test_dataset = test_dataset.reshape(
+        (test_dataset.shape[0], test_dataset.shape[2])
+    )
+    inv_yhat = concatenate((yhat, test_dataset), axis=1)
+
+    inv_yhat = scaler.inverse_transform(inv_yhat)
+    inv_yhat = inv_yhat[:, 0]
+    test_y = test_labels.reshape((len(test_labels), 1))
+
+    inv_y = concatenate((test_y, test_dataset), axis=1)
+    inv_y = scaler.inverse_transform(inv_y)
+    inv_y = inv_y[:, 0]
+
+    # calculate Mean Squared error
+    mae = mean_absolute_error(inv_y, inv_yhat)
+    print("Test Mean Squared Absolute Error: %.3f" % mae)
+
+    return history
+
+
+def predict_future_for_real_data(df):
+    model, scaler, _, _, _ = lstm(8)
+    test_dataset, test_labels = split_dataframe(df, ["Canteen"])
+    test_dataset = reshape_df(test_dataset)
+
+    yhat = model.predict(test_dataset)
+    test_dataset = test_dataset.reshape(
+        (test_dataset.shape[0], test_dataset.shape[2])
+    )
+    inv_yhat = concatenate((yhat, test_dataset), axis=1)
+
+    inv_yhat = scaler.inverse_transform(inv_yhat)
+    inv_yhat = inv_yhat[:, 0]
+
+    return inv_yhat
+
+
+def main():
+    history = predict_future_for_local_testing()
+    plot_history(history)
+
+
+if __name__ == "__main__":
+    main()
